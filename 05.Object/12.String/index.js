fcd73f11a12257e39287dec1e26572c7d6371bf9
NSGuliyev <nesimi018@yahoo.com>
2020-05-08 23:01:50 -0400

    Object exercise

---
+++ 05.Object/12.String/index.js
@@ -0,0 +1,15 @@
+// String primitive
+const message = "This is my first message";
+console.log("The number of charachters are", message.length);
+
+console.log(message[0])
+
+console.log(message.includes("my"))
+
+console.log(message.replace("first", "second"))
+console.log(message.toUpperCase(message));
+
+console.log(message.split(" "))
+
+// String object
+const another = new String("Hi");
