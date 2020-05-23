7657646b9f177f5dc2fbc539428fd82c9fa07f4b
NSGuliyev <nesimi018@yahoo.com>
2020-05-23 17:04:58 -0400

    CSS Float

--- 07.Functions/01.Function Declarations vs. Expressions/index.js
+++ 07.Functions/01.Function Declarations vs. Expressions/index.js
@@ -1,3 +1,11 @@
+/*
+In JavaScript there are two ways to define a function,
+we can use the FUNCTION DECLARATION syntax,
+or FUNCTION EXPRESSION which basically involves
+declaring variable or constant and then setting it to a function
+*/ 
+
+
 // Fuction Declaration
 function walk() {
   console.log("Walk");
---
+++ 07.Functions/02.Hoisting/index.js
@@ -0,0 +1,11 @@
+// Function Declaration 
+function walk() {
+  console.log("Walk");
+}
+
+
+// Anonymous Function Expression
+const run = function () {
+  console.log("run");
+};
+
