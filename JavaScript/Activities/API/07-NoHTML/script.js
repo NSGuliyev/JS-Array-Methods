9d2fb2128e68bfb06bf0c35a869406ae06e2a42a
NSGuliyev <nesimi018@yahoo.com>
2020-07-28 18:26:25 -0300

    API7

---
+++ JavaScript/Activities/API/07-NoHTML/script.js
@@ -0,0 +1,82 @@
+/*
+=============================
+Create all necessary Elements
+=============================
+*/
+
+var h1El = document.createElement("h1");
+var h2El = document.createElement("h2");
+
+var infoEl = document.createElement("div");
+var imgEl = document.createElement("img");
+var kittenEl = document.createElement("div");
+var nameEl = document.createElement("div");
+
+var favoriteEl = document.createElement("div");
+var listEl = document.createElement("ol")
+var li1 = document.createElement("li")
+var li2 = document.createElement("li")
+var li3 = document.createElement("li")
+var li4 = document.createElement("li")
+
+// Store our li elements in a variable
+var listItems = document.getElementsByTagName("li");
+
+
+
+/*
+=========================================
+Set the text content of relevant elements
+=========================================
+*/ 
+h1El.textContent = "Welcome to my page"
+h2El.textContent = "This HTML document was created using JavaScript and Chrome Dev Tools";
+kittenEl.textContent = "This is my kitten";
+nameEl.textContent = "His name is JAX";
+favoriteEl.textContent = "My Favorite foods are: ";
+li1.textContent = "Chicken Fingers";
+li2.textContent = "Pizza";
+li3.textContent = "Burgers";
+li4.textContent = "Sushi";
+
+
+/*
+==========================
+Append all of our elements
+==========================
+*/ 
+document.body.appendChild(h1El);
+document.body.appendChild(h2El);
+// Div Info section
+document.body.appendChild(infoEl);
+infoEl.appendChild(imgEl);
+infoEl.appendChild(kittenEl);
+infoEl.appendChild(nameEl);
+//  Div Favorite foods 
+document.body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl)
+listEl.appendChild(li1)
+listEl.appendChild(li2)
+listEl.appendChild(li3)
+listEl.appendChild(li4)
+
+/*
+=========================
+Style all of our elements
+=========================
+*/
+
+h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+h2El.setAttribute("style", "margin:auto; width:100%; text-align:center;");
+infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+imgEl.setAttribute("src", "http://placekitten.com/300/300");
+imgEl.setAttribute("height", 300);
+imgEl.setAttribute("width", 300);
+kittenEl.setAttribute("style", "font-size:25px; text-align:center");
+nameEl.setAttribute("style", "font-size:25px; text-align:center");
+favoriteEl.setAttribute("style", "font-size:20px");
+listEl.setAttribute("style", "background:#333333; padding:20px;");
+listItems[0].setAttribute("style", "color:white; background:#666666; padding:5px; margin-left:35px;");
+listItems[1].setAttribute("style", "color:white; background:#777777; padding:5px; margin-left:35px;");
+listItems[2].setAttribute("style", "color:white; background:#888888; padding:5px; margin-left:35px;");
+listItems[3].setAttribute("style", "color:white; background:#999999; padding:5px; margin-left:35px;");
